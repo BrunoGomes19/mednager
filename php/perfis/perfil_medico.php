870680e575f6ade0623f7774c0f593304347dd20
Bruno Gomes <bgomes18.1999@hotmail.com>
2018-11-07 20:15:45 +0000

    perfil_medico terminado

--- php/perfis/perfil_medico.php
+++ php/perfis/perfil_medico.php
@@ -1,9 +1,65 @@
 <?php
 include('../topos/topo_medico.php');
+
+$sql = "SELECT * from comprador where emailComprador like '$email'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+
+  $sexo = $row["sexoComprador"];
+
+  $nome = $row["nomeComprador"];
+
+  $especialidadeInt = $row["codEspecialidade"];
+
+  $especialidade = $row["codEspecialidade"];
+
+  if($especialidade==1){
+
+    $especialidade="";
+
+  }else{
+
+    if($especialidade==2){
+
+    $especialidade="Pediatria";
+
+  }else{
+
+    if($especialidade==3){
+
+    $especialidade="Cardiologia";
+
+  }else{
+
+    $especialidade="";
+
+  }
+
+  }
+
+  }
+
+  //sexo
+
+  $date = $row["dataNascComprador"];
+
+  $cidade = $row["localidadeComprador"];
+
+  $sobremim = $row["formacaoCarreira"];
+
+  }
+} else {
+  echo "Error";
+}
+$conn->close();
+
 ?>
 
             <!-- MAIN CONTENT-->
-              <link href="style.css" rel="stylesheet">
+              <link href="../../assets/css/style.css" rel="stylesheet">
 
             <div class="content" style="padding-top:8%;">
                         <div class="container-fluid">
@@ -24,77 +80,59 @@ include('../topos/topo_medico.php');
             																	</div>
                                         <div class="content">
                                             <form>
+                                              <div class="row">
+                                                  <div class="col-md-6">
+                                                      <div class="form-group">
+                                                          <label>Nome completo</label>
+                                                          <input type="text" class="form-control" value="<?php echo $nome; ?>" readonly>
+                                                      </div>
+                                                  </div>
+                                                  <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label>Endereço de e-mail</label>
+                                                        <input type="text" class="form-control" value="<?php echo $email; ?>" readonly>
+                                                    </div>
+                                                  </div>
+                                              </div>
+
                                                 <div class="row">
-                                                    <div class="col-md-5">
-                                                        <div class="form-group">
-                                                            <label>Company (disabled)</label>
-                                                            <input type="text" class="form-control" disabled placeholder="Company" value="Creative Code Inc.">
-                                                        </div>
+                                                    <div class="col-md-6">
+                                                      <div class="form-group">
+                                                          <label>Sexo</label>
+                                                          <input type="text" class="form-control" value="<?php echo $sexo; ?>" readonly>
+                                                      </div>
                                                     </div>
-                                                    <div class="col-md-3">
-                                                        <div class="form-group">
-                                                            <label>Username</label>
-                                                            <input type="text" class="form-control" placeholder="Username" value="michael23">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-4">
-                                                        <div class="form-group">
-                                                            <label for="exampleInputEmail1">Email address</label>
-                                                            <input type="email" class="form-control" placeholder="Email">
-                                                        </div>
+                                                    <div class="col-md-6">
+                                                      <div class="form-group">
+                                                          <label>Especialidade</label>
+                                                          <input type="text" class="form-control" value="<?php echo $especialidade; ?>" readonly>
+                                                      </div>
                                                     </div>
                                                 </div>
 
                                                 <div class="row">
                                                     <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label>First Name</label>
-                                                            <input type="text" class="form-control" placeholder="Company" value="Mike">
-                                                        </div>
+                                                      <div class="form-group">
+                                                          <label>Data de nascimento</label>
+                                                          <input type="text" class="form-control" value="<?php echo $date; ?>" readonly>
+                                                      </div>
                                                     </div>
                                                     <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label>Last Name</label>
-                                                            <input type="text" class="form-control" placeholder="Last Name" value="Andrew">
-                                                        </div>
+                                                      <div class="form-group">
+                                                          <label>Cidade</label>
+                                                          <input type="text" class="form-control" value="<?php echo $cidade; ?>" readonly>
+                                                      </div>
                                                     </div>
                                                 </div>
+
+
+
 
                                                 <div class="row">
                                                     <div class="col-md-12">
                                                         <div class="form-group">
-                                                            <label>Address</label>
-                                                            <input type="text" class="form-control" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09">
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                <div class="row">
-                                                    <div class="col-md-4">
-                                                        <div class="form-group">
-                                                            <label>City</label>
-                                                            <input type="text" class="form-control" placeholder="City" value="Mike">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-4">
-                                                        <div class="form-group">
-                                                            <label>Country</label>
-                                                            <input type="text" class="form-control" placeholder="Country" value="Andrew">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-4">
-                                                        <div class="form-group">
-                                                            <label>Postal Code</label>
-                                                            <input type="number" class="form-control" placeholder="ZIP Code">
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                <div class="row">
-                                                    <div class="col-md-12">
-                                                        <div class="form-group">
-                                                            <label>About Me</label>
-                                                            <textarea rows="5" class="form-control" placeholder="Here can be your description" value="Mike">Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</textarea>
+                                                            <label>Sobre mim</label>
+                                                            <textarea rows="5" class="form-control" readonly><?php echo $sobremim; ?></textarea>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -113,23 +151,16 @@ include('../topos/topo_medico.php');
                                                  <a href="#">
                                                 <img class="avatar border-gray" src="../../assets/images/users/1.jpg" alt="..."/>
 
-                                                  <h4 class="title">Mike Andrew<br />
-                                                     <small>michael24</small>
+                                                  <h4 class="title"><?php echo $nome; ?><br />
+                                                     <br>
                                                   </h4>
                                                 </a>
                                             </div>
-                                            <p class="description text-center"> "Lamborghini Mercy <br>
-                                                                Your chick she so thirsty <br>
-                                                                I'm in that two seat Lambo"
+                                            <p class="description text-center"> <?php echo $sobremim; ?>
                                             </p>
                                         </div>
                                         <hr>
-                                        <div class="text-center">
-                                            <button href="#" class="btn btn-simple"><i class="fa fa-facebook-square"></i></button>
-                                            <button href="#" class="btn btn-simple"><i class="fa fa-twitter"></i></button>
-                                            <button href="#" class="btn btn-simple"><i class="fa fa-google-plus-square"></i></button>
 
-                                        </div>
                                     </div>
                                 </div>
 
