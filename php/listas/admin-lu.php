96a1773765427e5a604212d1ba30ad6d6b858a9d
Patrícia Martinho <pa7amc@outlook.com>
2018-11-08 15:57:40 +0000

    desgin_lu

--- php/listas/admin-lu.php
+++ php/listas/admin-lu.php
@@ -31,40 +31,45 @@ include('../topos/topo_medico.php');
 
 
 
-                            <h3 class="title-5 m-b-35">data table</h3>
+                            <h3 class="title-5 m-b-35">Lista de utentes</h3>
                                     <div class="table-data__tool">
                                         <div class="table-data__tool-left">
-                                            <div class="rs-select2--light rs-select2--md">
-                                                <select class="js-select2" name="property">
-                                                    <option selected="selected">All Properties</option>
-                                                    <option value="">Option 1</option>
-                                                    <option value="">Option 2</option>
-                                                </select>
+                                            <div class="rs-select2--light ">
+                                                    
+                                                    
+                                                   <div class="input-group">
+                                                        
+                                                        <button class="btn btn-primary" disabled>
+                                                            <i class="fa fa-search"></i>
+                                                        </button>
+                                                        
+                                                        <input type="text" id="input1-group2" name="input1-group2" placeholder="Username" class="form-control">
+                                                    </div>
+
+
+
                                                 <div class="dropDownSelect2"></div>
                                             </div>
-                                            <div class="rs-select2--light rs-select2--sm">
-                                                <select class="js-select2" name="time">
-                                                    <option selected="selected">Today</option>
-                                                    <option value="">3 Days</option>
-                                                    <option value="">1 Week</option>
-                                                </select>
-                                                <div class="dropDownSelect2"></div>
-                                            </div>
-                                            <button class="au-btn-filter">
-                                                <i class="zmdi zmdi-filter-list"></i>filters</button>
+                                            
+                                            
                                         </div>
                                         <div class="table-data__tool-right">
                                             <button class="au-btn au-btn-icon au-btn--green au-btn--small">
-                                                <i class="zmdi zmdi-plus"></i>add item</button>
-                                            <div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
-                                                <select class="js-select2" name="type">
-                                                    <option selected="selected">Export</option>
-                                                    <option value="">Option 1</option>
-                                                    <option value="">Option 2</option>
-                                                </select>
-                                                <div class="dropDownSelect2"></div>
-                                            </div>
+                                                <i class="zmdi zmdi-plus"></i>adicionar utente</button>
+                                            
                                         </div>
+
+                                        <div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
+                                           
+                                                        
+                                                        <button class="btn btn-primary" disabled>
+                                                            <i class="fa fa-search"></i> Associados
+                                                        </button>
+                                                        
+                                                
+                                                    </div>
+                                        </div>
+
                                     </div>
                                     <div class="table-responsive table-responsive-data2">
                                         <table class="table table-data2">
@@ -76,12 +81,9 @@ include('../topos/topo_medico.php');
                                                             <span class="au-checkmark"></span>
                                                         </label>
                                                     </th>
-                                                    <th>name</th>
-                                                    <th>email</th>
-                                                    <th>description</th>
-                                                    <th>date</th>
-                                                    <th>status</th>
-                                                    <th>price</th>
+                                                    <th>Nome</th>
+                                                    <th>Cartão de cidadão</th>
+                                                    <th>NIF</th>
                                                     <th></th>
                                                 </tr>
                                             </thead>
