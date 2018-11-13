2b4482a7396979c414f2a8c84ccaf4fdb93ed18e
Bruno Gomes <bgomes18.1999@hotmail.com>
2018-11-13 14:39:02 +0000

    Lista de medicamentos terminada

--- php/listas/ajaxlistamedicamentos.php
+++ php/listas/ajaxlistamedicamentos.php
@@ -16,6 +16,17 @@ $descriEspecialidade = $_GET['descri'];
 
 $str = $_GET['str'];
 
+
+function tirarAcentos($str){
+    return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$str);
+}
+
+
+
+$str = tirarAcentos($str);
+
+
+
 $con = mysqli_connect('localhost','admin','Sutas4Ever2018','mydb');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
@@ -41,7 +52,7 @@ if (!$con) {
   }else{
 
     $sql="SELECT * from medicamento, especialidade, medicamentoespecialidade where medicamento.codMedicamento=medicamentoespecialidade.codMedicamento and especialidade.codEspecialidade=medicamentoespecialidade.codEspecialidade and especialidade.descriEspecialidade like '".$descriEspecialidade."'
-     and nomeMedicamento like '".$str."%'";
+     and nomeMedicamento like '".$str."%' limit 50";
 
      mysqli_select_db($con,"ajax_demo");
      $result = mysqli_query($con,$sql);
@@ -49,7 +60,7 @@ if (!$con) {
      if ($result->num_rows == 0) {
 
        $sql="SELECT * from medicamento, especialidade, medicamentoespecialidade where medicamento.codMedicamento=medicamentoespecialidade.codMedicamento and especialidade.codEspecialidade=medicamentoespecialidade.codEspecialidade and especialidade.descriEspecialidade like '".$descriEspecialidade."'
-        and nomeGenerico like '".$str."%'";
+        and nomeGenerico like '".$str."%' limit 50";
 
         mysqli_select_db($con,"ajax_demo");
         $result = mysqli_query($con,$sql);
