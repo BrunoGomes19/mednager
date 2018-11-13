a072d200bc7f84f280c43679617d62d8bf0fb567
Maria Correia <mariadpfdc@gmail.com>
2018-11-13 12:06:27 +0000

    Update php/pages_medico/charts.php

--- php/pages_medico/charts.php
+++ php/pages_medico/charts.php
@@ -5,7 +5,13 @@ include('../topos/topo_medico.php');
 
 ?>
 
-<meta charset="UTF-8">
+<meta charset="UTF-8">+
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 <!--formden.js communicates with FormDen server to validate fields and submit via AJAX -->
 <script type="text/javascript" src="https://formden.com/static/cdn/formden.js"></script>
 
@@ -27,13 +33,165 @@ include('../topos/topo_medico.php');
                     <div class="row">
                     <div class="col-md-12">
                         <div class="card">
-                            <div class="header">
 
-                                <?php
-                                include('charts.php');
-                                ?>
+                                
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            <div class="card-body">
+                                <h5 class="card-title">Real Time Chart</h5>
+                                 <div id="real-time" style="height:400px;"></div>
+                                <p>Time between updates:
+                                    <input id="updateInterval" type="text" value="" style="text-align: right; width:5em"> milliseconds
+                                </p>
                             </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ENd chart-1 -->
+                <!-- Chart-2 -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Turning-series chart</h5>
+                                <div id="placeholder" style="height: 400px;"></div>
+                                <p id="choices" class="m-t-20"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Chart-2 -->
+                <!-- Cards -->
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card m-t-0">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="peity_line_neutral left text-center m-t-10"><span><span style="display: none;">10,15,8,14,13,10,10</span>
+                                        <canvas width="50" height="24"></canvas>
+                                        </span>
+                                        <h6>10%</h6>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 border-left text-center p-t-10">
+                                    <h3 class="mb-0 font-weight-bold">150</h3>
+                                    <span class="text-muted">New Users</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card m-t-0">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="peity_bar_bad left text-center m-t-10"><span><span style="display: none;">3,5,6,16,8,10,6</span>
+                                        <canvas width="50" height="24"></canvas>
+                                        </span>
+                                        <h6>-40%</h6></div>
+                                </div>
+                                <div class="col-md-6 border-left text-center p-t-10">
+                                    <h3 class="mb-0 font-weight-bold">4560</h3>
+                                    <span class="text-muted">Orders</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card m-t-0">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="peity_line_good left text-center m-t-10"><span><span style="display: none;">12,6,9,23,14,10,17</span>
+                                        <canvas width="50" height="24"></canvas>
+                                        </span>
+                                        <h6>+60%</h6>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 border-left text-center p-t-10">
+                                    <h3 class="mb-0 ">5672</h3>
+                                    <span class="text-muted">Active Users</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card m-t-0">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="peity_bar_good left text-center m-t-10"><span>12,6,9,23,14,10,13</span>
+                                        <h6>+30%</h6>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 border-left text-center p-t-10">
+                                    <h3 class="mb-0 font-weight-bold">2560</h3>
+                                    <span class="text-muted">Register</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End cards -->
+                <!-- Chart-3 -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Bar Chart</h5>
+                                <div class="flot-chart">
+                                    <div class="flot-chart-content" id="flot-line-chart"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End chart-3 -->
+                <!-- Charts -->
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Pie Chart</h5>
+                                <div class="pie" style="height: 400px;"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Line Chart</h5>
+                                <div class="bars" style="height: 400px;"></div>
+                            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
                         </div>
                             
                                 
@@ -83,6 +241,39 @@ include('../topos/topo_medico.php');
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
+
+
+
+<!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="../../assets/libs/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../../assets/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="../../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="../../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="../../assets/extra-libs/sparkline/sparkline.js"></script>
+    <!--Wave Effects -->
+    <script src="../../dist/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="../../dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="../../dist/js/custom.min.js"></script>
+    <!-- this page js -->
+    <script src="../../assets/libs/chart/matrix.interface.js"></script>
+    <script src="../../assets/libs/chart/excanvas.min.js"></script>
+    <script src="../../assets/libs/flot/jquery.flot.js"></script>
+    <script src="../../assets/libs/flot/jquery.flot.pie.js"></script>
+    <script src="../../assets/libs/flot/jquery.flot.time.js"></script>
+    <script src="../../assets/libs/flot/jquery.flot.stack.js"></script>
+    <script src="../../assets/libs/flot/jquery.flot.crosshair.js"></script>
+    <script src="../../assets/libs/chart/jquery.peity.min.js"></script>
+    <script src="../../assets/libs/chart/matrix.charts.js"></script>
+    <script src="../../assets/libs/chart/jquery.flot.pie.min.js"></script>
+    <script src="../../assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
+    <script src="../../assets/libs/chart/turning-series.js"></script>
+    <script src="../../dist/js/pages/chart/chart-page-init.js"></script>
 </body>
 
 </html>
