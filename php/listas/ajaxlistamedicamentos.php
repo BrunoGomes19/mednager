03e65d86c839612b553aa9d31c9bd376ec607479
Bruno Gomes <bgomes18.1999@hotmail.com>
2018-11-13 11:35:32 +0000

    lista de medicamentos update

--- php/listas/ajaxlistamedicamentos.php
+++ php/listas/ajaxlistamedicamentos.php
@@ -24,13 +24,13 @@ if (!$con) {
 
   if($codEspecialidade==1){
 
-    $sql="SELECT * from medicamento, especialidade, medicamentoespecialidade where medicamento.codMedicamento=medicamentoespecialidade.codMedicamento and especialidade.codEspecialidade=medicamentoespecialidade.codEspecialidade and descriMedicamento like '".$str."%'";
+    $sql="SELECT * from medicamento, especialidade, medicamentoespecialidade where medicamento.codMedicamento=medicamentoespecialidade.codMedicamento and especialidade.codEspecialidade=medicamentoespecialidade.codEspecialidade and nomeMedicamento like '".$str."%'";
 
 
   }else{
 
     $sql="SELECT * from medicamento, especialidade, medicamentoespecialidade where medicamento.codMedicamento=medicamentoespecialidade.codMedicamento and especialidade.codEspecialidade=medicamentoespecialidade.codEspecialidade and especialidade.descriEspecialidade like '".$descriEspecialidade."'
-     and descriMedicamento like '".$str."%'";
+     and nomeMedicamento like '".$str."%'";
 
   }
 
@@ -43,9 +43,12 @@ if (!$con) {
   <table class="table table-data2">
       <thead>
           <tr>
-              <th></th>
-              <th>Nome</th>
-              <th>Especialidade</th>
+              <th>Nome genérico</th>
+              <th>Nome do medicamento</th>
+              <th>Forma farmacêutica</th>
+              <th>Dosagem</th>
+              <th style="width:2px">Genérico</th>
+              <th>Titular de AIM</th>
           </tr>
       </thead>
       <tbody>
@@ -57,22 +60,27 @@ if (!$con) {
 
   while($row = mysqli_fetch_array($result)) {
 
-  $descriMedicamento = $row['descriMedicamento'];
+  $nomeGenerico = $row['nomeGenerico'];
 
-  $descriEspecialidade = $row['descriEspecialidade'];
+  $nomeMedicamento = $row['nomeMedicamento'];
 
+  $formaFarmaceutica = $row['formaFarmaceutica'];
 
+  $dosagem = $row['dosagem'];
+
+  $generico = $row['generico'];
+
+  $titularAIM = $row['titularAIM'];
 
   echo '
     <tr class="spacer"></tr>
     <tr class="tr-shadow">
-        <td>
-
-        </td>
-        <td>'.$descriMedicamento.'</td>
-        <td>
-            <span class="block-email">'.$descriEspecialidade.'</span>
-        </td>
+        <td>'.$nomeGenerico.'</td>
+        <td>'.$nomeMedicamento.'</td>
+        <td>'.$formaFarmaceutica.'</td>
+        <td>'.$dosagem.'</td>
+        <td style="width:2px">'.$generico.'</td>
+        <td>'.$titularAIM.'</td>
 
 
         </td>
