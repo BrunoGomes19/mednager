0ef5258e22d0cba12a79bc34e24b8e6a365aed14
Patrícia Martinho <pa7amc@outlook.com>
2018-11-14 16:30:09 +0000

    chupem agora, afinal.
    remove já dá mas se carregarmos no editar ou cancelar qd abrimos evento, dá shit

--- php/calendarioFULL/celke.sql
+++ php/calendarioFULL/celke.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 14/11/2017 às 00:12
--- Versão do servidor: 5.7.14
--- Versão do PHP: 7.0.10
+-- Host: 127.0.0.1:3306
+-- Generation Time: 17-Ago-2018 às 21:39
+-- Versão do servidor: 5.7.21
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,56 +19,40 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `celke`
+-- Database: `celke`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `events`
+-- Estrutura da tabela `events`
 --
 
-CREATE TABLE `events` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(220) DEFAULT NULL,
   `color` varchar(10) DEFAULT NULL,
   `start` datetime DEFAULT NULL,
-  `end` datetime DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `end` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
--- Fazendo dump de dados para tabela `events`
+-- Extraindo dados da tabela `events`
 --
 
 INSERT INTO `events` (`id`, `title`, `color`, `start`, `end`) VALUES
-(1, 'Reuniao', '#0071c5', '2017-11-23 09:00:00', '2017-11-23 11:00:00'),
-(2, 'Palestra', '#40e0d0', '2017-11-13 14:00:00', '2017-11-13 17:00:00'),
-(3, 'Reuniao 1', '#FFD700', '2017-11-23 08:00:00', '2017-11-23 09:00:00'),
-(4, 'Reuniao 3', '#40e0d0', '2017-11-23 10:00:00', '2017-11-23 11:00:00'),
-(5, 'Reuniao 4', '#0071c5', '2017-11-23 11:00:00', '2017-11-13 12:00:00'),
-(6, 'Reuniao 5', '#FFD700', '2017-11-23 13:00:00', '2017-11-23 14:00:00'),
-(7, 'Reuniao 6', '#0071c5', '2017-11-23 14:00:00', '2017-11-23 15:00:00'),
-(8, 'Reuniao 7', '#FFD700', '2017-11-23 16:00:00', '2017-11-23 17:00:00');
+(1, 'Reuniao', '#0071c5', '2018-08-17 09:00:00', '2018-08-23 11:00:00'),
+(2, 'Palestra', '#40e0d0', '2018-08-13 14:00:00', '2018-08-13 17:00:00'),
+(3, 'Reuniao 1', '#FFD700', '2018-08-23 08:00:00', '2018-08-23 09:00:00'),
+(4, 'Reuniao 3', '#40e0d0', '2018-08-23 10:00:00', '2018-08-23 11:00:00'),
+(5, 'Reuniao 4', '#0071c5', '2018-08-23 11:00:00', '2018-08-13 12:00:00'),
+(6, 'Reuniao 5', '#FFD700', '2018-08-23 13:00:00', '2018-08-23 14:00:00'),
+(7, 'Reuniao 6', '#0071c5', '2018-08-23 14:00:00', '2018-08-23 15:00:00'),
+(8, 'Reuniao 7', '#FFD700', '2018-08-23 16:00:00', '2018-08-23 17:00:00');
+COMMIT;
 
---
--- Índices de tabelas apagadas
---
-
---
--- Índices de tabela `events`
---
-ALTER TABLE `events`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
---
-
---
--- AUTO_INCREMENT de tabela `events`
---
-ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- php/calendarioFULL/conexao.php
+++ php/calendarioFULL/conexao.php
@@ -1,8 +1,9 @@
 <?php
-	$servidor = "localhost";
-	$usuario = "admin";
-	$senha = "Sutas4Ever2018";
-	$dbname = "celke";
-	
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+$servidor = "localhost";
+$usuario = "admin";
+$senha = "Sutas4Ever2018";
+$dbname = "celke";
+
+//Criar a conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
