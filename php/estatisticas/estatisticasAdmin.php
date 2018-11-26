94e3bcc3a8e5d6a39e88bde0176c48baaae1588f
carolinaroqu3 <carolinaroqu3@gmail.com>
2018-11-26 17:08:41 +0000

    some charts

--- php/estatisticas/estatisticasAdmin.php
+++ php/estatisticas/estatisticasAdmin.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <?php
-  include('../topos/topo_medico.php');
+  include('../topos/topo_Admin.php');
   ?>
 
 </head>
@@ -14,20 +14,22 @@
   $sqlLEI = "select LEIComprador from comprador where emailComprador like '$emailComprador'";
 
 
-  $result = $conn->query($sql);
+  $result = $conn->query($sqlLEI);
 
   if ($result->num_rows > 0) {
     // output data of each row  
     
     while($row = $result->fetch_assoc()) {
       $LEIComprador = $row["LEIComprador"];
+
     }
 
   } else {
-    echo "Error";
+    echo "Error1";
   }
 
-  $sql = "select sexoUtente as descricao, count(*) as c from comprador, utente, associados where associados.comprador_codComprador=comprador.codComprador and associados.utente_ccUtente=utente.ccUtente and  emailComprador like '$emailComprador' group by sexoUtente";
+
+  $sql = "select sexoComprador as descricao, count(*) as c from comprador where LEIComprador='$LEIComprador' and codPermissao=2 group by sexoComprador";
 
   $result = $conn->query($sql);
 
@@ -39,25 +41,46 @@
     $contagem_nd=0;
     while($row = $result->fetch_assoc()) {
 
-      $sexoUtente = $row["descricao"];
-      if(strcmp($sexoUtente, "Feminino") == 0){
+      $sexoComprador = $row["descricao"];
+      if(strcmp($sexoComprador, "Feminino") == 0){
         $contagem_feminino = $row["c"];
       }
-      else if (strcmp($sexoUtente, "Outro") == 0){
+      else if (strcmp($sexoComprador, "Outro") == 0){
         $contagem_outro = $row["c"];
-      } else if (strcmp($sexoUtente, "Masculino") == 0){
+      } else if (strcmp($sexoComprador, "Masculino") == 0){
         $contagem_masculino = $row["c"];
       }
       else{
         $contagem_nd = $row["c"];
       }
-
     }
 
 
   } else {
     echo "Error";
   }
+
+
+
+  $sql2 = "select descriEspecialidade, COUNT(*) as nrMedicos from especialidade, comprador where comprador.codEspecialidade=especialidade.codEspecialidade and nrOrdem is not null and LEIComprador='$LEIComprador' group by descriEspecialidade";
+  $result2 = $conn->query($sql2);
+
+  if ($result2->num_rows > 0) {
+    $arrayEsp = array();
+  while($row = $result->fetch_assoc()) {
+
+    array_push($arrayEsp, $row["descriEspecialidade"], parseInt($row["nrMedicos"]));
+
+  }
+
+  } else {
+  echo "Error";
+  }
+
+
+
+
+  $conn->close();
 
 
   ?>
@@ -71,7 +94,7 @@
 
     <div class="row" >
       <div class="col-md-12" >
-        <div id="piechart" style="width: 1170px; height: 500px;"></div>
+        <div id="piechart" style="width: 970px; height: 500px;"></div>
 
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
@@ -89,12 +112,58 @@
         ]);
 
         var options = {
-        title: '% Sexo dos utentes associados'
+        title: '% Sexo dos médicos associados'
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
         chart.draw(data, options);
+        }
+        </script>
+
+
+
+
+        <div id="chart_div" style="width: 970px; height: 130px;"></div>
+
+        <script type="text/javascript">
+        google.charts.load('current', {packages: ['corechart', 'bar']});
+        google.charts.setOnLoadCallback(drawBasic);
+
+        function drawBasic() {
+
+          var data = google.visualization.arrayToDataTable([
+
+            ['Especialidade', 'Número de médicos',],
+            <?php
+            if($result2->num_rows > 0){
+                while($row = $result2->fetch_assoc()){
+                  echo "['".$row['descriEspecialidade']."', ".$row['nrMedicos']."],";
+                }
+            }
+            ?>
+          ]);
+
+          var options = {
+            title: 'Número de médicos por especialidade',
+            vAxis: {
+              textStyle : {
+                fontSize: 13 // or the number you want
+
+              }
+            },
+
+            hHaxis: {
+              chxs: '0N*f0',
+            },
+
+            chartArea:{left:200,top:40,bottom: 40,width:"30%",height:"100%"},
+
+          };
+
+          var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+          chart.draw(data, options);
         }
         </script>
 
