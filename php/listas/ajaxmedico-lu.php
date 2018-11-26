da2bb8381788fc1cf74fba379e4d53a43b73fbd2
Patrícia Martinho <pa7amc@outlook.com>
2018-11-26 17:55:14 +0000

    Update ajaxmedico-lu.php

--- php/listas/ajaxmedico-lu.php
+++ php/listas/ajaxmedico-lu.php
@@ -115,7 +115,8 @@ echo '
 
               <button class="btn btn-outline-primary" onclick="verperfil('.$cc.');">
                   <i class="fa fa-user"></i>&nbsp;Perfil</button>
-              
+                  <button class="btn btn-outline-primary" >
+                      <i class="fa fa-calendar-plus"></i>&nbsp;Plano</button>
 
       </td>
   </tr>
