dd8b795559467dd569a4d450d347c630e4fa2194
Bruno Gomes <bgomes18.1999@hotmail.com>
2018-12-10 10:12:53 +0000

    asd

--- php/indexes/index-medico.php
+++ php/indexes/index-medico.php
@@ -133,7 +133,7 @@ if ($result->num_rows > 0) {
 
                         <div class="row m-t-25">
                             <div class="col-sm-6 col-lg-2">
-                                <div class="overview-item overview-item--c1" style="height:27%;">
+                                <div class="overview-item overview-item--c1" style="height:30%;">
                                     <div class="overview__inner">
                                         <div class="overview-box clearfix">
                                             <div class="icon">
@@ -148,7 +148,7 @@ if ($result->num_rows > 0) {
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-2">
-                                <div class="overview-item overview-item--c2" style="height:27%;">
+                                <div class="overview-item overview-item--c2" style="height:30%;">
                                     <div class="overview__inner">
                                         <div class="overview-box clearfix">
                                             <div class="icon">
@@ -164,7 +164,7 @@ if ($result->num_rows > 0) {
                                 </div>
                             </div>
                             <div class="col-sm-8 col-lg-2">
-                                <div class="overview-item overview-item--c3" style="height:27%;">
+                                <div class="overview-item overview-item--c3" style="height:30%;">
                                     <div class="overview__inner">
                                         <div class="overview-box clearfix">
                                             <div class="icon">
@@ -182,7 +182,7 @@ if ($result->num_rows > 0) {
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-2">
-                                <div class="overview-item overview-item--c4" style="height:27%;">
+                                <div class="overview-item overview-item--c4" style="height:30%;">
                                     <div class="overview__inner">
                                         <div class="overview-box clearfix">
                                             <div class="icon">
