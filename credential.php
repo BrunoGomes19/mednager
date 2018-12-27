68cd60234c6312cedb34b02090a1eaec6f5bec11
Patrícia Martinho <pa7amc@outlook.com>
2018-12-27 19:33:42 +0000

    RARei o projeto com a pass do costume

--- credential.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	define('EMAIL','mednagerresetpass@gmail.com');
-	define('PASS','BCMPresetpass18');
-	
-?>
--- php/topos/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$hostname='localhost';
-	$user = 'admin';
-	$password = 'Sutas4Ever2018';
-	$mysql_database = 'mydb';
-	$conn = mysqli_connect($hostname, $user, $password,$mysql_database);
-
-
-?>
